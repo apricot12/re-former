f4c2c64d71f1f4aca120bacf5e7a55866382591a
Nihal Mohammed <mnihal64@outlook.com>
2021-03-09 18:07:27 +0530

    made editpage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,9 @@ class UsersController < ApplicationController
         @user = User.new(user_params)
 
         if @user.save
-        redirect_to new_user_path
+            redirect_to new_user_path
         else
-        render :new
+            render :new
         end
     end
 
